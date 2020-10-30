d3c8e58b777772410d513fb446071a5c4a361a2d
Uibirá <usbarreto@gmail.com>
2020-10-30 00:13:38 -0300

    Refatoração do exercício 1

---
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_1/exercicio-01a.js
@@ -0,0 +1,10 @@
+/*let a = 5;
+let b = 2;
+soma = a + b;
+console.log("Adção: ", soma);*/
+
+//Refatoração
+function sum(a, b) {
+    return a + b
+}
+console.log(sum(5, 5));
--- bloco_4/introducao-a-javascript-e-logica-de-programacao_1/exercicio-01b.js
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_1/exercicio-01b.js
@@ -1,4 +1,10 @@
-let a = 5;
+/*let a = 5;
 let b = 2;
 subtrai = a - b;
-console.log("Subtração: ", subtrai);
+console.log("Subtração: ", subtrai);*/
+
+//Refatoração 
+function sub(a, b) {
+    return a - b
+}
+console.log(sub(5,2));
